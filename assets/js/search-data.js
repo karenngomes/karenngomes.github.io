92dbc98bc1398bf34740a198fae116fb661c60ee
karenngomes <karenngomes.dev@gmail.com>
2026-07-07 02:10:35 +0000

    Deploying to gh-pages from @ karenngomes/karenngomes.github.io@8ef2862f02cf081305cee497109b433ec4070e30 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          description: "Software Developer, Master&#39;s student in Computer Science at UFMG.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
